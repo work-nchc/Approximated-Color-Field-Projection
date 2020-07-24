a03dfd401a4931a843fca8269fb0eebb3c807c9b
1803031 <37138507+work-nchc@users.noreply.github.com>
2020-07-24 18:08:48 +0800

    Add files via upload

--- realdraw.py
+++ realdraw.py
@@ -1,9 +1,10 @@
 from open3d import io
 from ncfp import board, pinhole
-from numpy import array
+from numpy import array, save
 from pyglet.window import Window
 from pyglet.image import ImageData
 from pyglet.app import run
+from PIL.Image import fromarray
 from time import time
 
 t = time()
@@ -11,15 +12,23 @@ cloud = io.read_point_cloud('test.ply')
 print(time() - t, 0)
 print(cloud)
 
-t = time()
 board_hd = board()
-center = array((400., 1100., 400.))
-quat = array((1., 0., 0., 0.))
-print(time() - t, 1)
+#center = array((400., 1100., 400.))
+#quat = array((1., 0., 0., 0.))
+center = array((475., 1100., 50.))
+quat = array((0.70710678, 0., 0., 0.70710678))
 
 t = time()
-board_hd.proj_o3d(cloud, center, quat, pinhole())
+board_hd.proj_o3d(cloud, center, quat, pinhole(), 0.08)
+print(time() - t, 1)
+
+save('test', board_hd.data)
+
+t = time()
+img = fromarray(board_hd.image()[::-1], 'RGB')
 print(time() - t, 2)
+
+img.save('test.png')
 
 t = time()
 window = Window(fullscreen=True)
@@ -28,6 +37,8 @@ print(time() - t, 3)
 t = time()
 image = ImageData(board_hd.width, board_hd.height, 'RGB', bytes(board_hd))
 print(time() - t, 4)
+
+image.save('test2.png')
 
 @window.event
 def on_draw():
