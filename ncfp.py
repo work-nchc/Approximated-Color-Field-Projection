0330a889a1a535d65202a0fc836f89f1aef0fd4e
1803031 <37138507+work-nchc@users.noreply.github.com>
2020-07-29 14:10:09 +0800

    Add files via upload

--- ncfp.py
+++ ncfp.py
@@ -33,12 +33,14 @@ def pinhole(f=_width/2., px=_width/2., py=_height/2.):
 class board(object):
     
     def __init__(
-        self, width=_width, height=_height, fields=_fields, dtype=float
+        self, width=_width, height=_height, fields=_fields, dtype=float,
+        back_weight=float_info.min
     ):
         self.width = width = max(int(width), 1)
         self.height = height = max(int(height), 1)
         self.fields = fields = max(int(fields), 0)
         self.data = zeros((height, width, fields + 2), dtype)
+        self.back_weight = max(float(back_weight), float_info.min)
     
     def __iadd__(self, value):
         self.data += value.data
@@ -46,46 +48,48 @@ class board(object):
     def image(self):
         return (
             self.data[:, :, :self.fields] /
-            (self.data[:, :, -1:] + float_info.min) *
+            (self.data[:, :, -1:] + self.back_weight) *
             color_max
         ).astype('uint8')
     
     def __bytes__(self):
         return self.image().tobytes()
     
-    def draw_pix(self, x, y, r, dist, color, alpha):
-        weight = alpha * cross_sec(r) * dist ** exponent_decay
+    def draw_pix(self, x, y, r, dist, color, decay):
+        weight = cross_sec(r) * decay
         self.data[y, x, :self.fields] += color * weight
         self.data[y, x, self.fields] += dist * weight
         self.data[y, x, -1] += weight
     
-    def draw_quad(self, xi, yi, dx, dy, x0, y0, radius, dist, color, alpha):
+    def draw_quad(self, xi, yi, dx, dy, x0, y0, radius, dist, color, decay):
         x = xi
         y = yi
         r = ((x - x0) ** 2 + (y - y0) ** 2) ** 0.5
         while (r < radius or x != xi) and 0 <= y < self.height:
             if r < radius and 0 <= x < self.width:
-                self.draw_pix(x, y, r / radius, dist, color, alpha)
+                self.draw_pix(x, y, r / radius, dist, color, decay)
                 x += dx
             else:
                 x = xi
                 y += dy
             r = ((x - x0) ** 2 + (y - y0) ** 2) ** 0.5
     
-    def draw(self, x0, y0, dist, color, alpha, camera, radius_pt):
+    def draw(self, x0, y0, dist, color, alpha, size_pt):
         dist = max(dist, near_clip)
-        radius = max(radius_pt * camera[1, 1] / dist, radius_min)
+        radius = max(size_pt / dist, radius_min)
+        decay = alpha * dist ** exponent_decay
         x0i = int(x0)
         y0i = int(y0)
         x0 -= 0.5
         y0 -= 0.5
-        self.draw_quad(x0i, y0i, 1, 1, x0, y0, radius, dist, color, alpha)
-        self.draw_quad(x0i, y0i-1, 1, -1, x0, y0, radius, dist, color, alpha)
-        self.draw_quad(x0i-1, y0i, -1, 1, x0, y0, radius, dist, color, alpha)
-        self.draw_quad(x0i-1, y0i-1, -1, -1, x0, y0, radius, dist, color, alpha)
+        self.draw_quad(x0i, y0i, 1, 1, x0, y0, radius, dist, color, decay)
+        self.draw_quad(x0i, y0i-1, 1, -1, x0, y0, radius, dist, color, decay)
+        self.draw_quad(x0i-1, y0i, -1, 1, x0, y0, radius, dist, color, decay)
+        self.draw_quad(x0i-1, y0i-1, -1, -1, x0, y0, radius, dist, color, decay)
     
-    def proj_pt(self, pt, center, quat, camera, radius_pt):
-        v_cam = Rotation(quat).apply(pt[:3] - center, True)
+    def proj_pt(self, pt, center, rotation, camera, size_pt):
+        #v_cam = rotation.apply(pt[:3] - center, True)
+        v_cam = rotation @ (pt[:3] - center)
         if 0. < v_cam[2]:
             v = camera @ v_cam
             x0 = v[0] / v[2]
@@ -93,15 +97,26 @@ class board(object):
             if 0. <= x0 < self.width and 0. <= y0 < self.height:
                 self.draw(
                     x0, y0, (v_cam @ v_cam) ** 0.5, pt[3:self.fields+3], pt[-1],
-                    camera, radius_pt)
+                    size_pt)
     
     def proj(self, cloud, center, quat, camera, radius_pt):
-        {self.proj_pt(pt, center, quat, camera, radius_pt) for pt in cloud}
-    
-    def proj_o3d(self, cloud, center, quat, camera, radius_pt):
+        rotation = Rotation(quat).inv().as_matrix()
+        size_pt = radius_pt * camera[1, 1]
         {
             self.proj_pt(
-                concatenate((*pt, array((1.,)))), center, quat, camera,
-                radius_pt
-            ) for pt in zip(cloud.points, cloud.colors)
+                pt, center, rotation, camera, size_pt
+            ) for pt in cloud
+        }
+    
+    def proj_o3d(self, cloud, center, quat, camera, radius_pt):
+        rotation = Rotation(quat).inv().as_matrix()
+        size_pt = radius_pt * camera[1, 1]
+        {
+            self.proj_pt(
+                concatenate((*pt, array((1.,)))), center, rotation,
+                camera, size_pt
+            ) for pt in zip(
+                cloud.points,
+                cloud.colors
+            )
         }
